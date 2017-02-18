b7ba840efe1ecdf2436350dec83c1294f7abec5d
jdbriner07 <jdbriner07>
2017-02-17 17:32:46 -0800

    stuck on sending messages to the server

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,24 +1,34 @@
 // YOUR CODE HERE:
 //http://parse.sfm6.hackreactor.com/
 
+// var message = {
+//   username: 'shawndrost',
+//   text: 'trololo',
+//   roomname: '4chan'
+// };
+
+
 var app = {
-  server: 'http://parse.sfm6.hackreactor.com/'
+  server: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages'
 };
 
 app.init = function () {
-  $(document).on('click', '.username', app.handleUsernameClick);
   // $(document).on('submit', '#send .submit', app.handleSubmit);
-  $('#send .submit').submit(app.handleSubmit);
+  $(document).on('click', '.username', app.handleUsernameClick);
+  $(document).on('click', '#send .submit', app.handleSubmit);
+  //$(document).submit(app.handleSubmit);
+
+  // $('#send .submit').submit(app.handleSubmit);
 };
 
 app.send = function (message) {
   $.ajax({
-    url: 'http://parse.sfm6.hackreactor.com/',
+    url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
     type: 'POST',
     data: JSON.stringify(message),
-    contentType: 'application/json',
+    contentType: 'jsonp',
     success: function (data) {
-      console.log('chatterbox: Message sent');
+      console.log('chatterbox: Message sent', data);
     },
     error: function (data) {
       console.error('chatterbox: Failed to send message', data);
@@ -29,13 +39,11 @@ app.send = function (message) {
 
 app.fetch = function () {
   $.ajax({
-    url: 'http://parse.sfm6.hackreactor.com/',
+    url: 'http://parse.sfm6.hackreactor.com/chatterbox/classes/messages',
     type: 'GET',
     // data: 
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('chatterbox: Message sent');
-    },
+    contentType: 'jsonp',
+    success: app.parseMessage,
   });
 };
 
@@ -43,10 +51,16 @@ app.clearMessages = function () {
   $('#chats').empty();
 };
 
+app.parseMessage = function (messageArray) {
+  console.log('messages recieved');
+  // messageArray.forEach(app.renderMessage);
+  app.clearMessages();  
+  _.each(messageArray.results, app.renderMessage);
+};
+
 app.renderMessage = function (message) {
   // $('#chats').append('<p>' + message + '</p>');
-  $('#chats').append(`<p class="username">${message}</p>`);
-
+  $('#chats').append(`<p class="chat"><a class="username">${message.username}: </a> ${message.text}</p>`);
 };
 
 app.renderRoom = function (room) {
@@ -55,14 +69,23 @@ app.renderRoom = function (room) {
 
 
 app.handleUsernameClick = function () {
-
+  console.log('username clicked');
 };
 
 app.handleSubmit = function () {
+  var message = {username: 'Jeff',
+    text: $('.message')[0].value,
+    roomname: 'lobby'
+  };
+  console.log(message);
+  app.send(message);
+  // return false;
+};
 
-}
+app.init();
 
-
+app.fetch();
+setInterval(app.fetch, 5000);
 
 
 
